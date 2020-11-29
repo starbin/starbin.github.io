8be4d32842bdf7e1445fca739e8cd4e9a94786b4
bintang raga <>
2020-11-29 23:55:35 +0700

    add gallery

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,3 +10,18 @@ $(window).scroll(function() {
         $('.navbar').css("top", "-100%");
     }
 });
+
+var btn = $('#back-btn');
+
+        $(window).scroll(function() {
+        if ($(window).scrollTop() > 300) {
+            btn.addClass('show');
+        } else {
+            btn.removeClass('show');
+        }
+        });
+
+        btn.on('click', function(e) {
+            e.preventDefault();
+            $('html, body').animate({scrollTop:0}, '300');
+        });
